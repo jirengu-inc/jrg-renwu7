462c9c03c6b4a128fbdf33fba81c42a212e4365b
hongdagen <570483535@qq.com>
2016-09-26 15:01:14 +0800

    test24-1new

--- members/洪毅/TEST24/24-1/24-1.js
+++ members/洪毅/TEST24/24-1/24-1.js
@@ -1,45 +1,44 @@
-function ajax(opts){
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function(){
-		if(xmlhttp.readystate==4&&xmlhttp.status==200){
-			var json=JSON.parse(xmlhttp.responseText)
-			opts.success(json);
-		}
-		if(xmlhttp.readystate==4&&xmlhttp.status==404){
-			opts.error();
-		}
-	}
-	var strdata="";
-	for(var key in opts.data){
-		strdata+=key+"="+opts.data[key]+"&";
-	}
-	strdata=strdata.substr(0,strdata.length-1);
-	if(opts.type.toLowerCase()==="get"){
-		xmlhttp.open("get",opts.url+"?"+strdata,true);
-		xmlhttp.send();
-	}
-	if(opts.type.toLowerCase()==="post"){
-		xmlhttp.open("post",opts.url,true);
-		xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-		xhr.send(strdata);
-	}
+function ajax(opts) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var json = JSON.parse(xmlhttp.responseText)
+            opts.success(json);
+        }
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 404) {
+            opts.error();
+        }
+    }
+    var strdata = "";
+    for (var key in opts.data) {
+        strdata += key + "=" + opts.data[key] + "&";
+    }
+    strdata = strdata.substr(0, strdata.length - 1);
+    if (opts.type.toLowerCase() === "get") {
+        xmlhttp.open('get', opts.url + "?" + strdata, true);
+        xmlhttp.send();
+    }
+    if (opts.type.toLowerCase() === "post") {
+        xmlhttp.open('post', opts.url, true);
+        xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xmlhttp.send(strdata);
+    }
 }
-document.getElementById("btn").addEventListener('click', function(){
+document.querySelector('#btn').addEventListener('click', function() {
     ajax({
-        url: 'getData.php',   //接口地址
-        type: 'get',
-			dataType:'json',
+        url: 'getData.php', //接口地址
+        type: 'get', // 类型， post 或者 get,
         data: {
             username: 'xiaoming',
             password: 'abcd1234'
         },
-        success: function(ret){
+        success: function(ret) {
             console.log(ret);
-            var mydiv=document.querySelector("#mydiv");
-            mydiv.innerHTML="用户名 "+ret.username+"密码"+ret.password;
+            var mydiv = document.querySelector('#mydiv');
+            mydiv.innerHTML = '用户名：' + ret.username + " <br/> " + '密码：' + ret.password;
         },
-        error: function(){
-           console.log('出错了')
+        error: function() {
+            console.log('出错了');
         }
     })
 });
--- members/洪毅/TEST24/24-1/getData.php
+++ members/洪毅/TEST24/24-1/getData.php
@@ -1,7 +1,7 @@
 <?php
-header(":Content-type:");
-$username=$_GET["username"];
-if($username==="xiaoming"){
+	header("Content-type: ");
+	$username = $_GET['username'];
+	if($username === 'xiaoming'){
 		$ret = array('username'=>'xiaoming', 'password'=>'abcd1234');
 	} else {
 		$ret = array('username'=>'blank', 'password'=>'blank');
