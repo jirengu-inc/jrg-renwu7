98a6461f24c6f890bed03f1d933eca896f45d825
暑假项目 <1041874421@qq.com>
2016-11-09 19:49:04 +0800

    finish task38

---
+++ members/邹立杰/AMD-CMD/js/inc.js
@@ -0,0 +1,16 @@
+define(function(){
+	var num = (function(){
+		var a = 1;
+		function inc(){
+			a++;
+		}
+		function get(){
+			return a
+		}
+		return {
+			inc:inc,
+			get:get
+		}
+	})()
+	return num;
+})
