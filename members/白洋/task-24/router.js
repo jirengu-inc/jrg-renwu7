862bd69c97c1bb6ce29b501926127b7558b5da9d
free <free@free.lan>
2016-12-14 14:35:19 +0800

    task-24

--- members/白洋/task-24/router.js
+++ members/白洋/task-24/router.js
@@ -1,45 +1,4 @@
-
-
-
-
-/**
- * 使用范例
- */
-
-
-/**
- * 发送 GET 请求， 无参数
- * GET /hello
- * 返回响应数据
- */
-app.get('/hello', function(req, res) {
-	res.send({
-		status: 0,
-		msg: "hello 饥人谷"
-	});
-});
-
-
-/**
- * 发送 GET 请求, 有参数
- * GET /user/100
- * query = { name: 'ruoyu', age: 28 }
- */
-app.get('/user/:uid', function(req, res) {
-	console.log(req.params.uid); //100
-	console.log(req.query.name); // 'ruoyu'
-	res.send({
-		status: 1,
-		errorMsg: "请先注册"
-	});
-});
-
-
-/**
- * 发送 POST 请求， 有参数
- * POST /comment
- * query = { comment: "这是评论内容" }
- */
+// 任务一
 app.post('/comment', function(req, res) {
 	console.log(req.body.comment); // "这是评论内容"
 	res.send({
@@ -51,15 +10,15 @@ app.post('/comment', function(req, res) {
 	});
 });
 
+// 任务二
 
-
-/**
- * 页面路由，从模板渲染页面渲染页面, 
- * htttp://localhost:8080/user
- * 支持 ejs, jade 模板
- */
-app.get('/user', function(req, res) {
-	res.render('user.ejs', {
-		username: '饥人谷'
+app.get('/list', function(req, res) {
+	console.log("no404");
+	res.send({
+		status: 0,
+		data: {
+			list: 100
+		}
 	});
 });
+
