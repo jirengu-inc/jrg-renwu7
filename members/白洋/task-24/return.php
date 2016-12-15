a512a622d15000aafec3eac02b5ee2e93259ee11
free <free@free.lan>
2016-12-15 21:07:21 +0800

    task-24-03

--- members/白洋/task-24/return.php
+++ members/白洋/task-24/return.php
@@ -24,6 +24,20 @@ echo json_encode($tdata);
 }
 if($getid==3){
 // 代码3
+$gettype=$_GET['type'];
+$getshuju=$_GET['shuju'];
+if($gettype=='user'){
+	if($getshuju=='test'){
+		echo json_encode('0');
+	}else{
+		echo json_encode('1');
+	}
+
+}
+if($gettype=='all'){
+//密码就不判断了
+	echo json_encode('注册成功');
+}
 
 }
 
