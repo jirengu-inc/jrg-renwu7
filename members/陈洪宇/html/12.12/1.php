bdf364bfc4b5b2633c8d5764957c2a71a29ad69e
Diego-Chen-hy <diegochenhy@gmail.com>
2016-12-18 14:53:58 +0800

    12.18.1

---
+++ members/陈洪宇/html/12.12/1.php
@@ -0,0 +1,10 @@
+<?php
+
+	$start = $_GET['start'];
+	$len = $_GET['len'];
+	$items = array();
+	for($i = 0; $i < $len; $i++){
+	array_push($items, '内容' . ($start+$i+1));
+	}
+	echo json_encode($items);
+?>
