bed09457c82497d65e987521c811f39bbdb7b6cd
jack <cjichao@163.com>
2016-12-30 00:41:57 +0800

    espusure

--- members/陈继超/task39/js/main.js
+++ members/陈继超/task39/js/main.js
@@ -5,7 +5,7 @@ require.config({
 		'jquery': 'lib/jquery.min'
 	}
 });
-require(['jquery','app/fullScreenCarousel','app/goTop','app/waterFull','app/Exposure'], function($,Carousel,GoTop,waterFull,Exposure){
+require(['jquery','app/fullScreenCarousel','app/goTop','app/waterFull','app/exposure'], function($,Carousel,GoTop,waterFull,Exposure){
 		GoTop.init();
 		$('.load-more').click();
 		Exposure.init($('#about .exposure'));//.init($('#about .vertical-line li'))
