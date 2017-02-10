d129fbf1c0ff61a04f9a5ae003d74c7f71916a4d
jrg-rw7 <1041874421@qq.com>
2017-02-10 11:51:50 +0800

    task postcss

--- members/邹立杰/task47-less/gulpfile.js
+++ members/邹立杰/task47-less/gulpfile.js
@@ -1,9 +1,24 @@
 
 var gulp = require("gulp");
 var less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('less', function () {
   return gulp.src('./less/**/*.less')
     .pipe(less())
+    .pipe(autoprefixer({
+    	browsers: ['not ie <= 8','Firefox <= 20'],
+    	cascade: false
+    }))
     .pipe(gulp.dest('./dist/css'));
 });
+
+gulp.task('css', () =>
+    gulp.src('css/css3.css')
+        .pipe(autoprefixer({
+            browsers: ['not ie <= 8','Firefox <= 20'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./dist/css'))
+);
+gulp.task("all",["less","css"])
