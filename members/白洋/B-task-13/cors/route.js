3f1a0fcbd7d266d657c48be4a29d65ab41dba86c
free <free@free.lan>
2017-02-11 17:32:35 +0800

    task-b-13

---
+++ members/白洋/B-task-13/cors/route.js
@@ -0,0 +1,24 @@
+/**
+ * Created by codelike on 17-2-11.
+ */
+
+var http = require("http"),
+    url = require("url");
+
+
+http.createServer(function (req, res) {
+    if(req.url==="/favicon.ico"){
+        console.log("nullssss");
+    }else
+    {
+        console.log(req.url);
+        var params = url.parse(req.url,true).query;
+        console.log(params.news);
+        res.writeHeader(200,{'Access-Control-Allow-Origin': '*'});
+        // res.writeHeader(200, {'Content-Type':'text/javascript;charset=UTF-8'});
+        res.write("news("+'"'+params.news+'"'+");");
+        res.end();
+    }
+
+
+}).listen(9999, "127.0.0.1");
